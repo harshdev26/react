19ca63f901f900a940b4dc987fcfe8280cd1c941
harshdev26 <156781178+harshdev26@users.noreply.github.com>
2025-07-11 12:12:06 +0000

    .......

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,12 @@
+
+
+function App() {
+  return (
+    <h1>chai aur react| My self Harsh Tiwari 
+      In this lecture i have stuudied about how to use create framework
+      how to study dependencies all etc..
+    </h1>
+  );
+}
+
+export default App;
