53cd7a9c3204662d6223afe7e9cbbad684937469
harshdev26 <156781178+harshdev26@users.noreply.github.com>
2025-07-12 09:10:17 +0000

    ...............

---
+++ practice.js
@@ -0,0 +1,40 @@
+// // function customRender(reactElement, Container){
+// //   domElement = document.createElement(reactElement.type)
+// //   domElement.innerHTML = reactElement.children
+// //   domElement.setAttribute("href", reactElement.props.href)
+// //   domElement.setAttribute("target", reactElement.props.target)
+// // }
+
+// // container.appendChild(domElement)
+
+
+// domElement = document.createElement(reactElement.type)
+// domElement.innerHTML = reactElement.children
+// for(const prop in reactElement.props){
+//     if(prop == 'children'){
+//         domElement.setAttribute(prop, reactElement.props[prop] )
+//     }
+//     mainContainer.appendChild(domElement)
+// }
+
+
+
+
+
+
+
+
+
+
+
+// const reactElement = {
+//     type: 'a',
+//     props: {
+//         href: "https://google.com",
+//         target: "_blank"
+//     },
+//     children: " click me to visit google"
+// }
+
+// mainContainer = document.querySelector("#root")
+// customRender(reactElement, mainCont)
