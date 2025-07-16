85fdc704e8eeb311ae3b651cb11fe27ae2ae6ecb
harshdev26 <156781178+harshdev26@users.noreply.github.com>
2025-07-16 08:52:46 +0000

    .............

--- 07reactRouter/src/Layout.jsx
+++ 07reactRouter/src/Layout.jsx
@@ -3,15 +3,14 @@ import Header from './components/Header/Header'
 import Footer from './components/Footer/Footer'
 import { Outlet } from 'react-router-dom'
 
-function Layout(){
-    return (
-        <>
-         <Header/>    
-         <Outlet/>
-         <Footer/>
-
-        </>
-    )
+function Layout() {
+  return (
+    <>
+    <Header/>
+    <Outlet />
+    <Footer />
+    </>
+  )
 }
 
 export default Layout
